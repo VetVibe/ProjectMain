5ac86c8570b85585337b2c7365b8b96c9d3235fe
AlinaZ <93255163+AlinaZahozi@users.noreply.github.com>
2024-01-31 22:53:01 +0200

    updated IP address

--- src/screens/TipsScreenPet/TipsScreenPet.js
+++ src/screens/TipsScreenPet/TipsScreenPet.js
@@ -10,7 +10,7 @@ export default function TipsScreenPet({ route, navigation }) {
   useEffect(() => {
     const fetchAllTips = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/tips/all');
+        const response = await axios.get('http://10.0.2.2:3000/tips/all');
         if (response.data) {
           setVetTips(response.data);
         }
